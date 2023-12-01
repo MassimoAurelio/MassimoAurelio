d37782bff35acef6be5f01bc7e11f768d8b352cb
MassimoAurelio <massimoaureliowork@gmail.com>
2023-12-01 18:21:02 +0300

    update 01.12 №2

--- src/components/about/MoreToConnect.vue
+++ src/components/about/MoreToConnect.vue
@@ -18,13 +18,13 @@ const socialsStore = useMySocialsStore()
       </div>
 
       <div class="socialButtons">
-        <ul>
-          <li v-for="item in socialsStore.items" :key="item.label">
-            <a :href="item.link"
-              ><div>
-                <span><img :src="item.imgSrc" alt="" /></span><img src="" alt="" />{{ item.label }}
-              </div></a
-            >
+        <ul class="ul">
+          <li class="button" v-for="item in socialsStore.items" :key="item.label">
+            <a class="link" :href="item.link" target="_blank">
+              <div class="buttonValue">
+                <span><img :src="item.imgSrc" alt="" /></span>{{ item.label }}
+              </div>
+            </a>
           </li>
         </ul>
       </div>
@@ -35,26 +35,60 @@ const socialsStore = useMySocialsStore()
 <style lang="scss" scoped>
 @import '@/assets/variables';
 
-.content {
-  max-width: 100%;
-  height: auto;
+.firstContainer {
   @extend .flex-column;
-  justify-content: center;
-  align-items: center;
   gap: 3vh;
 
-  p {
-    padding: 3%;
-  }
-  .avatar {
-    width: 20vh;
-  }
-  .description {
-    padding-left: 3%;
-    padding-right: 3%;
+  .content {
+    max-width: 100%;
+    height: auto;
+    padding: 3vh;
     @extend .flex-column;
     justify-content: center;
     align-items: center;
+    gap: 3vh;
+
+    .avatar {
+      width: 20vh;
+    }
+    .description {
+      padding-left: 5%;
+      padding-right: 5%;
+      @extend .flex-column;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
+  .socialButtons {
+    width: 100%;
+    @extend .flex-column;
+    .ul {
+      @extend .flex-column;
+      gap: 2vh;
+
+      .button {
+        width: 100%;
+        @extend .flex-row;
+        align-items: center;
+        background-color: #222222;
+        border-radius: 8px;
+        height: 5vh;
+        padding-left: 1vh;
+        .link {
+          display: block;
+          text-decoration: none;
+          color: inherit;
+        }
+
+        .buttonValue {
+          width: 100%;
+          @extend .flex-row;
+          align-items: center;
+          gap: 1vh;
+        }
+      }
+    }
   }
 }
 </style>
